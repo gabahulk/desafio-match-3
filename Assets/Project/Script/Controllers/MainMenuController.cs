dfa04d4464e6f0f58e3e6fcab8f62b95e2ba697d
Gaba <gabrielfpaula@gmail.com>
2026-09-16 22:07:59 -0300

    Add animated main menu

---
+++ Assets/Project/Script/Controllers/MainMenuController.cs
@@ -0,0 +1,143 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Gazeus.DesafioMatch3.Controllers
+{
+    public sealed class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Image _background;
+        [SerializeField] private RectTransform[] _suits;
+        [SerializeField] private RectTransform _joker;
+        [SerializeField] private RectTransform _title;
+        [SerializeField] private CanvasGroup _titleCanvasGroup;
+        [SerializeField] private RectTransform _playButtonTransform;
+        [SerializeField] private CanvasGroup _playButtonCanvasGroup;
+        [SerializeField] private Button _playButton;
+
+        private readonly Vector2[] _suitEntryDirections =
+        {
+            new Vector2(-1.0f, 1.0f),
+            new Vector2(1.0f, 1.0f),
+            new Vector2(-1.0f, -1.0f),
+            new Vector2(1.0f, -1.0f)
+        };
+
+        private bool _introComplete;
+
+        private void Awake()
+        {
+            _playButton.interactable = false;
+            _playButton.onClick.AddListener(Play);
+        }
+
+        private void Start()
+        {
+            PlayIntro();
+        }
+
+        private void OnDestroy()
+        {
+            _playButton.onClick.RemoveListener(Play);
+        }
+
+        public void Play()
+        {
+            if (_introComplete)
+            {
+                SceneManager.LoadScene("Gameplay");
+            }
+        }
+
+        private void PlayIntro()
+        {
+            RectTransform canvas = (RectTransform)transform;
+            Vector2 canvasSize = canvas.rect.size;
+            Color backgroundColor = _background.color;
+            backgroundColor.a = 0.45f;
+            _background.color = backgroundColor;
+
+            Vector2[] suitFinalPositions = new Vector2[_suits.Length];
+            for (int index = 0; index < _suits.Length; index++)
+            {
+                RectTransform suit = _suits[index];
+                suitFinalPositions[index] = suit.anchoredPosition;
+                Vector2 direction = _suitEntryDirections[index];
+                suit.anchoredPosition += Vector2.Scale(direction, canvasSize * 0.5f);
+                suit.localScale = Vector3.one * 0.9f;
+                suit.localRotation = Quaternion.Euler(0.0f, 0.0f, direction.x * 14.0f);
+            }
+
+            Vector2 jokerFinalPosition = _joker.anchoredPosition;
+            _joker.anchoredPosition += Vector2.down * canvasSize.y * 0.12f;
+            _joker.localScale = Vector3.one * 0.7f;
+            _joker.localRotation = Quaternion.Euler(0.0f, 0.0f, -10.0f);
+
+            _title.localScale = Vector3.one * 0.9f;
+            _titleCanvasGroup.alpha = 0.0f;
+
+            Vector2 playFinalPosition = _playButtonTransform.anchoredPosition;
+            _playButtonTransform.anchoredPosition += Vector2.down * canvasSize.y * 0.16f;
+            _playButtonTransform.localScale = Vector3.one * 0.9f;
+            _playButtonCanvasGroup.alpha = 0.0f;
+
+            Sequence intro = DOTween.Sequence();
+            Color visibleBackground = _background.color;
+            visibleBackground.a = 1.0f;
+            intro.Join(DOTween.To(() => _background.color, color => _background.color = color,
+                visibleBackground, 0.2f));
+
+            for (int index = 0; index < _suits.Length; index++)
+            {
+                RectTransform suit = _suits[index];
+                float delay = index * 0.08f;
+                intro.Insert(delay, DOTween.To(() => suit.anchoredPosition,
+                    position => suit.anchoredPosition = position, suitFinalPositions[index], 0.32f)
+                    .SetEase(Ease.OutBack));
+                intro.Insert(delay, suit.DOScale(Vector3.one, 0.32f).SetEase(Ease.OutBack));
+                intro.Insert(delay, suit.DORotate(Vector3.zero, 0.32f, RotateMode.Fast).SetEase(Ease.OutQuad));
+            }
+
+            intro.Insert(0.3f, DOTween.To(() => _joker.anchoredPosition,
+                position => _joker.anchoredPosition = position, jokerFinalPosition, 0.35f).SetEase(Ease.OutBack));
+            intro.Insert(0.3f, _joker.DOScale(Vector3.one, 0.35f).SetEase(Ease.OutBack));
+            intro.Insert(0.3f, _joker.DORotate(Vector3.zero, 0.35f, RotateMode.Fast).SetEase(Ease.OutQuad));
+            intro.Insert(0.58f, _joker.DOPunchScale(Vector3.one * 0.1f, 0.16f, 4));
+
+            intro.Insert(0.36f, _title.DOScale(Vector3.one, 0.24f).SetEase(Ease.OutQuad));
+            intro.Insert(0.36f, Fade(_titleCanvasGroup, 1.0f, 0.24f));
+
+            intro.Insert(0.6f, DOTween.To(() => _playButtonTransform.anchoredPosition,
+                position => _playButtonTransform.anchoredPosition = position, playFinalPosition, 0.3f)
+                .SetEase(Ease.OutBack));
+            intro.Insert(0.6f, _playButtonTransform.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBack));
+            intro.Insert(0.6f, Fade(_playButtonCanvasGroup, 1.0f, 0.2f));
+            intro.OnComplete(() => BeginIdle(playFinalPosition, jokerFinalPosition));
+        }
+
+        private void BeginIdle(Vector2 playPosition, Vector2 jokerPosition)
+        {
+            _introComplete = true;
+            _playButton.interactable = true;
+            DOTween.To(() => _joker.anchoredPosition,
+                    position => _joker.anchoredPosition = position,
+                    jokerPosition + Vector2.up * 8.0f,
+                    1.6f)
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo);
+            DOTween.To(() => _playButtonTransform.anchoredPosition,
+                    position => _playButtonTransform.anchoredPosition = position,
+                    playPosition + Vector2.up * 3.0f,
+                    1.4f)
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+
+        private static Tween Fade(CanvasGroup canvasGroup, float endValue, float duration)
+        {
+            return DOTween.To(() => canvasGroup.alpha, value => canvasGroup.alpha = value, endValue, duration);
+        }
+    }
+}
---
+++ Assets/Project/Tests/PlayMode/MainMenuPlayModeTests.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using Gazeus.DesafioMatch3.Controllers;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Gazeus.DesafioMatch3.Tests.PlayMode
+{
+    public sealed class MainMenuPlayModeTests
+    {
+        [UnityTest]
+        public IEnumerator MainMenu_PlaysIntroBeforeEnablingPlay()
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+            yield return null;
+
+            GameObject canvas = GameObject.Find("Canvas");
+            GameObject suitGroup = GameObject.Find("SuitGroup");
+            GameObject joker = GameObject.Find("Joker");
+            GameObject playButtonObject = GameObject.Find("PlayButton");
+            Assert.That(canvas.GetComponent<MainMenuController>(), Is.Not.Null);
+            Assert.That(GameObject.Find("Background").GetComponent<Image>().sprite, Is.Not.Null);
+            Assert.That(GameObject.Find("Title").GetComponent<TMP_Text>().text, Is.EqualTo("MATCH 3"));
+            Assert.That(suitGroup.transform.Find("Heart"), Is.Not.Null);
+            Assert.That(suitGroup.transform.Find("Diamond"), Is.Not.Null);
+            Assert.That(suitGroup.transform.Find("Club"), Is.Not.Null);
+            Assert.That(suitGroup.transform.Find("Spade"), Is.Not.Null);
+            Assert.That(joker.GetComponent<Image>().sprite, Is.Not.Null);
+
+            Button playButton = playButtonObject.GetComponent<Button>();
+            Assert.That(playButton.interactable, Is.False);
+
+            yield return new WaitForSecondsRealtime(1.2f);
+
+            Assert.That(playButton.interactable, Is.True);
+            Assert.That(playButtonObject.transform.Find("Plaque").GetComponent<Image>().sprite, Is.Not.Null);
+            Assert.That(playButtonObject.transform.Find("Text").GetComponent<TMP_Text>().text, Is.EqualTo("PLAY"));
+        }
+    }
+}
